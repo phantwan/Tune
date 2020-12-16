e5c3fe64f8d9355d20ffcda404f127aae8b4ce32
Phantwan <Bingyu.cheng@mail.polimi.it>
2020-12-16 04:35:51 +0100

    updated photowall info

--- js/demo.js
+++ js/demo.js
@@ -98,3 +98,26 @@
 		}
 	});
 })(jQuery);
+
+
+//from floating buttons
+setTimeout(() => document.body.classList.add('render'), 60);
+const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+const navigate = (linkEl) => {
+	document.body.classList.remove('render');
+	document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+};
+navdemos.forEach(link => link.addEventListener('click', (ev) => {
+	ev.preventDefault();
+	navigate(ev.target);
+}));
+
+import Cursor from '../cursor';
+import ButtonCtrl from './buttonCtrl';
+
+// initialize custom cursor
+const cursor = new Cursor(document.querySelector('.cursor'));
+const button = new ButtonCtrl(document.querySelector('.button'));
+
+button.on('enter', () => cursor.enter());
+button.on('leave', () => cursor.leave());
